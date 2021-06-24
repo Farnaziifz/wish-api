4cad99842e1cfc13ce137fb64a7ff0a73230b0d7
farnazii <ffarzipour92@gmail.com>
2021-06-24 23:04:36 +0430

    upload file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,   
+  UseInterceptors,
+  UploadedFile,
+  UploadedFiles,
+  Post,
+  Param,
+  Res
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from './uploads/upload.utils';
 
 @Controller()
 export class AppController {
@@ -8,5 +18,51 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedFile(@UploadedFile() file) {
+    const response = {
+      statusCode: 200,
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+    return response;
+  }
+
+  @Post('multiple')
+  @UseInterceptors(
+    FilesInterceptor('image', 20, {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.forEach((file) => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileReponse);
+    });
+    return response;
+  }
+
+  @Get('upload/:imgpath')
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    return res.sendFile(image, { root: './uploads' });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
 
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useFindAndModify: false,
+    }),
+    MulterModule.register({
+      dest: './uploads',
     }),
     UserModule,
     AuthModule,
